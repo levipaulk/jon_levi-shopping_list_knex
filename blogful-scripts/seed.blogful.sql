6d58361797f54a9369d05607f573dfddbd3a213e
Levi Paulk <levipaulk@gmail.com>
2019-05-07 17:34:31 -0400

    add shopping-list-service and shopping-list-service.spec.js files

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Article 1',   now() - '1 day'::INTERVAL,  'Content 1'),
+    ('Article 2',   now() - '2 days'::INTERVAL,  'Content 2'),
+    ('Article 3',   now() - '3 days'::INTERVAL,  'Content 3'),
+    ('Article 4',   now() - '4 days'::INTERVAL,  'Content 4'),
+    ('Article 5',   now() - '5 days'::INTERVAL,  'Content 5'),
+    ('Article 6',   now() - '6 days'::INTERVAL,  'Content 6'),
+    ('Article 7',   now() - '7 days'::INTERVAL,  'Content 7'),
+    ('Article 8',   now() - '8 days'::INTERVAL,  'Content 8'),
+    ('Article 9',   now() - '9 days'::INTERVAL,  'Content 9'),
+    ('Article 10',   now() - '10 days'::INTERVAL,  'Content 10'),
+
+    ('Article 11',   now() - '11 days'::INTERVAL,  'Content 11'),
+    ('Article 12',   now() - '12 days'::INTERVAL,  'Content 12'),
+    ('Article 13',   now() - '13 days'::INTERVAL,  'Content 13'),
+    ('Article 14',   now() - '14 days'::INTERVAL,  'Content 14'),
+    ('Article 15',   now() - '15 days'::INTERVAL,  'Content 15'),
+    ('Article 16',   now() - '16 days'::INTERVAL,  'Content 16'),
+    ('Article 17',   now() - '17 days'::INTERVAL,  'Content 17'),
+    ('Article 18',   now() - '18 days'::INTERVAL,  'Content 18'),
+    ('Article 19',   now() - '19 days'::INTERVAL,  'Content 19'),
+    ('Article 20',   now() - '20 days'::INTERVAL,  'Content 20')
+;
